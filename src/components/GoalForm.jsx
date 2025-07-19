df67a135a3a8d80a3a72e158a4da9861a862cf3c
moen <moenr49@gmail.com>
2025-07-20 01:09:42 +0300

    add GoalForm component with input fields for goal details and styling in main.css

--- src/components/GoalForm.jsx
+++ src/components/GoalForm.jsx
@@ -1,5 +1,27 @@
 function GoalForm(){
     return(
+        <div id= "goalForm">
+             <div id= "goalName">
+                <labele>Your goal  </labele>
+                <input type="text" placeholder="Enter your goal here!" />
+            </div>
+            <div id= "goalTarget">
+                <labele>Target Ammount </labele>
+                <input type="number" placeholder="Enter your target amount here!" />
+            </div>
+            <div id= "goalCategory">
+                <lable>Category </lable>
+                <input type="text" placeholder="Enter the category of your goal here!" />
+            </div>
+            <div id= "goalDeadline">
+                <label>Deadline of the target </label>
+                <input type="text" placeholder="sample: 2023-12-31" />                
+            </div>            
+            <div >
+                <button id="goalButton">Add +</button>
+            </div>
+        </div>
+        
        
     )
 }
