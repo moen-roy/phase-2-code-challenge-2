15ea7aa44428c11fed307ddf7afb68e557479f9a
moen <moenr49@gmail.com>
2025-07-20 01:30:05 +0300

    add GoalDelete component and integrate it into main application

---
+++ src/components/GoalItem.jsx
@@ -0,0 +1,8 @@
+function GoalDelete(){
+    return(
+        <div>
+            <button id= "delete">Delete</button>
+        </div>
+    )
+}
+export default GoalDelete;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import GoalForm from './components/GoalForm'
+import GoalDelete from './components/GoalItem'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <GoalForm />
+   <GoalDelete />
   </StrictMode>,
 )
