11cfc87db0d19cf13defd211ee16a78be8ee6fc9
moen <moenr49@gmail.com>
2025-07-20 21:54:44 +0300

    refactor main.jsx to correct component import and render GoalForm only

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import GoalForm from './components/GoalForm'
-import GoalDelete from './components/GoalItem'
+import Goalchange from './components/GoalItem'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <GoalForm />
-   <GoalDelete />
+   {/* <Goalchange /> */}
   </StrictMode>,
 )
