8ac8be89deca3d91ccf46316fa0ff63c537e00e6
moen <moenr49@gmail.com>
2025-07-20 21:49:10 +0300

    refactor GoalForm component to include input validation and data submission logic

--- src/components/GoalForm.jsx
+++ src/components/GoalForm.jsx
@@ -1,28 +1,75 @@
+import { useState } from "react";
+import GoalView from "./GoalItem";
+
 function GoalForm(){
-    return(
-        <div id= "goalForm">
+  const [name, setName] = useState("");
+  const [targetAmount, setTargetAmount] = useState("");
+  const [category, setCategory] = useState("");
+  const [deadline, setDeadline] = useState("");
+
+   function Insertion(){
+        // Stop the program if any field is empty
+        if (!name || !targetAmount || !category || !deadline) {
+    alert("Please fill in all the fields before submitting.");
+    return; 
+  }
+        const newGoal = {
+      name,
+      targetAmount,
+      category,
+      deadline,
+      savedAmount: 0,
+      createdAt: new Date().toISOString().split("T")[0],
+    };
+
+    // fetching data from the json and adding a goal 
+        fetch(`http://localhost:3001/goals`, {
+      method: "POST",
+      //enables the server to know data is bieng added to the json file
+      headers: {
+        "Content-Type": "application/json"
+      },
+       body: JSON.stringify(newGoal)
+    })
+        .then(()=>{
+            alert("Successfull goal set");
+        //refreshes the page once the data is submitted
+            window.location.reload()        
+    })
+    // when the fatch fails, the error is displayed
+    .catch((err)=> console.log("New goal set failure",err))
+    };
+
+     return(
+        // this is a form for adding the new goal
+        //once the button is clicked, the funtion commences
+        <div>
+            <div id= "goalForm">
              <div id= "goalName">
-                <labele>Your goal  </labele>
-                <input type="text" placeholder="Enter your goal here!" />
+                <label>Your goal  </label>
+                <input type="text" placeholder="Enter your goal here!" onChange={(e)=> setName(e.target.value)} />
             </div>
             <div id= "goalTarget">
-                <labele>Target Ammount </labele>
-                <input type="number" placeholder="Enter your target amount here!" />
+                <label>Target Ammount </label>
+                <input type="number" placeholder="Enter your target amount here!" onChange= {(e)=> setTargetAmount(e.target.value)} />
             </div>
             <div id= "goalCategory">
-                <lable>Category </lable>
-                <input type="text" placeholder="Enter the category of your goal here!" />
+                <label>Category </label>
+                <input type="text" placeholder="Enter the category of your goal here!" onChange={(e)=> setCategory(e.target.value)} />
             </div>
             <div id= "goalDeadline">
                 <label>Deadline of the target </label>
-                <input type="text" placeholder="sample: 2023-12-31" />                
+                <input type="text" placeholder="sample: 2023-12-31" onChange={(e)=> setDeadline(e.target.value)} />                
             </div>            
-            <div >
-                <button id="goalButton">Add +</button>
-            </div>
+              <button id="goalButton" onClick={Insertion} >Add +</button>
         </div>
+            <GoalView />
+        </div>
+        
         
        
     )
 }
+
+
 export default GoalForm;
